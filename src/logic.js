adf7c5eb645e06fdcceeb20939bdc1fc7e63bc7b
Asunder <trashemail3301@gmail.com>
2025-05-30 16:01:49 -0800

    Build: Gameboard, Players
    Gameboard class added that logs hits, missses, ships sunk, and victory. Player class added with human player and computer, each player has thier own copy of the gameboard.

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,4 @@
-export { Ship };
+export { Ship, Gameboard, Player };
 
 class Ship {
     hits = 0;
@@ -20,4 +20,112 @@ class Ship {
             return false;
         }
     }
+}
+
+class Gameboard {
+    ships = [];
+    shipsToPlace = [5, 4, 3, 3, 2];
+    misses = [];
+    hits = [];
+
+    constructor() {};
+
+    placeShip(shipSize, position, isVertical = true) {
+        if (
+            (isVertical && position[1] - shipSize < -1) ||
+            (!isVertical && position[0] + shipSize > 10)
+        ) {
+            return false;
+        } else {
+            const shipCoordinates = this.calculateShipCoords(shipSize, position, isVertical);
+
+            for (const coord of shipCoordinates) {
+                for (const ship of this.ships) {
+                    for (const existingCoord of ship.coordinates) {
+                        if (coord[0] === existingCoord[0] && coord[1] === existingCoord[1]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            this.ships.push(new Ship(shipSize, shipCoordinates));
+            return true;
+        }
+    }
+
+    calculateShipCoords(shipSize, position, isVertical) {
+        const shipCoordinates = [];
+        let currentPosition = position;
+        if (isVertical) {
+            for (let i = 0; i < shipSize; i++) {
+                shipCoordinates.push([currentPosition[0], currentPosition[1]]);
+                currentPosition[1] -= 1;
+            }
+        } else {
+            for (let i = 0; i < shipSize; i++) {
+                shipCoordinates.push([currentPosition[0], currentPosition[1]]);
+                currentPosition[0] += 1;
+            }
+        }
+        return shipCoordinates;
+    }
+
+    receiveAttack(attackPosition) {
+        const returnObject = {
+            valid: true,
+            hit: false,
+            sunk: false,
+            won: false,
+        }
+
+        this.hits.forEach((hits) => {
+            if (attackPosition[0] === hits[0] && attackPosition[1] === hits[1]) {
+                returnObject.valid = false;
+            }
+        });
+
+        if (returnObject.valid === true) {
+            this.misses.forEach((misses) => {
+                if (
+                    attackPosition[0] === misses[0] &&
+                    attackPosition[1] === misses[1]
+                ) {
+                    returnObject.valid = false;
+                }
+            });
+
+            if (returnObject.valid === false) {
+                return returnObject;
+            }
+
+            for (let i = 0; i < this.ships.length; i++) {
+                for (let u = 0; u < this.ships[i].coordinates.length; u++) {
+                    if (
+                        this.ships[i].coordinates[u][0] === attackPosition[0] &&
+                        this.ships[i].coordinates[u][1] === attackPosition[1]
+                    ) {
+                        this.ships[i].hit();
+                        this.hits.push(attackPosition);
+                        returnObject.hit = true;
+                        if (this.ships[i].isSunk()) {
+                            returnObject.sunk = true;
+                            returnObject.won = true;
+                            this.ships.forEach((ship) => {
+                                if (!ship.isSunk) {
+                                    returnObject.won = false;
+                                    return;
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+        }
+        this.misses.push(attackPosition);
+        return returnObject;
+    }
+}
+
+class Player {
+
 }
--- src/logic.test.js
+++ src/logic.test.js
@@ -1,4 +1,5 @@
-import { Ship } from "./logic";
+import { experiments } from "webpack";
+import { Ship, Gameboard } from "./logic";
 
 // test if ship sinks
 test("sink ship", () => {
@@ -17,3 +18,40 @@ test("ship is not sunk", () => {
     expect(ship.isSunk()).toBe(false);
 })
 
+// test valid vertical ship 
+test("place valid vertical ship", () => {
+    const board = new Gameboard();
+    expect(board.placeShip(3, [5, 5], true)).toBe(true);
+});
+
+// test invalid vertical ship
+test("place invalid vertical ship", () => {
+    const board = new Gameboard();
+    expect(board.placeShip(3, [5, 0], true)).toBe(false);
+});
+
+// test valid horizontal ship
+test("place valid horizontal ship", () => {
+    const board = new Gameboard();
+    expect(board.placeShip(3, [5, 5], false)).toBe(true);
+});
+
+// test invalid horizontal ship
+test("place invalid horizontal ship", () => {
+    const board = new Gameboard;
+    expect(board.placeShip(3, [9, 5], false)).toBe(false)
+});
+
+// test hit
+test("hit ship", () => {
+    const board = new Gameboard();
+    board.placeShip(3, [0, 2], true);
+    expect(board.receiveAttack([0, 2])).toBe(true);
+});
+
+// test miss
+test("ship not hit", () => {
+    const board = new Gameboard();
+    board.placeShip(3, [0, 2], true);
+    expect(board.receiveAttack([0, 3])).toBe(false);
+})
